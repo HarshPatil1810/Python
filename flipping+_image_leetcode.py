268350b602de4ed68001ce9c9c7e773cf592ea34
Harshvardhan Patil <152369369+HarshPatil1810@users.noreply.github.com>
2025-04-20 13:30:40 +0530

    Create flipping+_image_leetcode.py

---
+++ flipping+_image_leetcode.py
@@ -0,0 +1,8 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for r in image:
+            r.reverse()
+            for i in range(len(r)):
+                r[i]=1-r[i]
+                
+        return image
